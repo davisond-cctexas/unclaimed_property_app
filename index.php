b9804ed2932f0f9d0f2974787674bffb1713c1e3
davisond-cctexas <davisond@cctexas>
2018-08-15 15:16:01 -0500

    made lib files relative

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
     <meta name="author" content="">
     <title>Unclaimed Property Application</title>
     <link href="form-validation.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">        
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="lib/bootstrap.min.css">        
+    <script src="lib/jquery.min.js"></script>
+    <script src="lib/bootstrap.min.js"></script>
     <style>
         .table-condensed{ font-size: 14px !important;}
         .container{ max-width: 90% !important}
